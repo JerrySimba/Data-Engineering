d4ad47e45b59128033dfe6d4b1241879ada2a1ea
JerrySimba <salibursalibur3@gmail.com>
2024-10-08 12:42:59 +0300

    define headers
    #define headers, which allows you to access the website servers as a non-robot and a safe party

---
+++ web-scraping with Python (BautifulSoup, Pandas, requests)/define_headers.py
@@ -0,0 +1,12 @@
+#define URL
+URL = "https://www.amazon.com/s?k=playstation+5&crid=2N4Y6I8K95YEV&sprefix=plays%2Caps%2C409&ref=nb_sb_ss_w_hit-vc-lth_playstation-5_k0_1_5"
+
+#define headers, which allows you to access the website servers as a non-robot and a safe party
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Connection': 'keep-alive',
+    'DNT': '1',
+    'Upgrade-Insecure-Requests': '1'
+}
