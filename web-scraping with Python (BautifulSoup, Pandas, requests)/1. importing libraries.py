7d54ccfe4e34abcc56ffebfb0b148b8fb8de329b
JerrySimba <salibursalibur3@gmail.com>
2024-10-08 18:39:13 +0300

    Update 1. importing libraries.py

--- web-scraping with Python (BautifulSoup, Pandas, requests)/1. importing libraries.py
+++ web-scraping with Python (BautifulSoup, Pandas, requests)/1. importing libraries.py
@@ -1,4 +1,4 @@
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
+import pandas as pd 
 import re regular expressions. #This module provides a powerful way to work with text and allows you to search, match, and manipulate strings using patterns. 
