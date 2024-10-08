3732a8ede5cbb4c8c1ba651c9204c770c0d28099
JerrySimba <salibursalibur3@gmail.com>
2024-10-08 13:05:11 +0300

    testing functionality

---
+++ web-scraping with Python (BautifulSoup, Pandas, requests)/5. testing.py
@@ -0,0 +1,4 @@
+#printing 'PlayStation®5 console (slim) with EA SPORTS College Football 25' to test if all libraries are in place
+
+ps5_text = soup.find('span', class_='a-size-medium a-color-base a-text-normal').text
+
