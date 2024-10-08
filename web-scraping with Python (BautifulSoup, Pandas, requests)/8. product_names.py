63c44f309b36a11b7fdc1fc45c292a2c4fbcfd8f
JerrySimba <salibursalibur3@gmail.com>
2024-10-08 13:15:35 +0300

    Create 8. product_names.py

---
+++ web-scraping with Python (BautifulSoup, Pandas, requests)/8. product_names.py
@@ -0,0 +1,29 @@
+all_elements = soup.select('.a-size-mini.a-color-base .a-size-medium.a-color-base.a-text-normal')
+
+for element in all_elements:
+    print(element.text.strip())
+
+# listing all PS5 product names: PlayStation 5 Console (PS5) in page 1
+
+# PlayStation®5 console (slim) with EA SPORTS College Football 25
+# PlayStation 5 Digital Edition - CFI-1102B
+# Xbox Series X Console (Renewed)
+# PlayStation DualSense Wireless Controller - Sterling Silver
+# $150 PlayStation Store Gift Card [Digital Code]
+# PlayStation DualSense® Wireless Controller - White
+# EA SPORTS College Football 25 - PlayStation 5
+# $250 PlayStation Store Gift Card [Digital Code]
+# Metaphor: ReFantazio Collector's Edition - PlayStation 5
+# PlayStation Pulse Elite Wireless Headset
+# Gaming Console Backpack for PS5 Slim, Travel Carry Case Bag for Playstation 5 Slim Console, Storage Backpack Compatible for PS5 Slim Disc & Digital Edition, Controllers, Game Cards & Accessories, Grey
+# 2TB SN850P NVMe M.2 SSD Officially Licensed Storage Expansion for PS5 Consoles, up to 7,300MB/s, with heatsink - WDBBYV0020BNC-WRSN
+# PlayStation®5 Digital Edition (Slim) – (Renewed Premium)
+# Madden NFL 25 - PlayStation 5
+# MARVEL’S SPIDER-MAN 2 – PS5 Standard Edition
+# PS5 Wall Mount Kit with Charging Station, Compatible with PS5/PS5 Slim Disc & Digital Version, Dual Controller Chargers for Playstation 5 Accessories with Multi-Mode RGB Light and Headphone Hanger
+# $200 PlayStation Store Gift Card [Digital Code]
+# Wireless Gaming Headset for PS5, PS4, PC, Mac, Switch, 2.4GHz Gaming Headsets with Bluetooth 5.4, Hidden ENC Noise Canceling Mic, 35H Battery, 3D Spatial Sound, 3.5mm Wired
+# PS5 / PS5 Slim Stand and Cooling Station, Controller Charging Station for Playstation 5 Console, PS5 Stand with Controller Charger for Disc & Digital, PS5 Holder with Cooling Fan and 3 USB Hubs
+# Controller Charger for PS5/PS5 Slim: PS5 Stand Cooling Fan and Controller Charger Station, PS5/PS5 Slim Accessories Vertical Charging Dock for PlayStation 5 Slim Console (White)
+# Undisputed - PlayStation 5
+# Selection deleted
